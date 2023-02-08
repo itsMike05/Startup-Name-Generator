56d28e467e46aeb7740555d55aa0146925f78e9f
Michał Pelc <michal.pelc@live.com>
2023-02-08 15:29:20 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Startup Name Generator',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNextWord() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var state = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Your startup idea:'),
+          Text(state.current.asLowerCase),
+          ElevatedButton(
+              onPressed: () {
+                state.getNextWord();
+              },
+              child: Text("Next!"))
+        ],
+      ),
+    );
+  }
+}
